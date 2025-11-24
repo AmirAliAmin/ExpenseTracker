c13ac6edee3558a4d1b9ef55fec22f100e7a9815
AmirAliAmin <aliaminamir77@gmail.com>
2025-11-24 16:00:30 +0500

    update

--- frontend/src/utils/apiPath.js
+++ frontend/src/utils/apiPath.js
@@ -1,5 +1,4 @@
-export const BASE_URL =  "http://localhost:8000"||import.meta.env.BASE_URL;
-
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 //untils.apiPath.js
 export const API_PATH = {
     AUTH:{
